e1f40dabf9d1d6b007f0b728ab5b1f7c66c4b3d4
Helgust <egortutun@gmail.com>
2019-04-26 20:18:21 +0300

    Add Parser_Add GitIgnore

--- Lex.cpp
+++ Lex.cpp
@@ -353,6 +353,42 @@ ostream & operator<< (ostream &s, Lex l){
 
 /////////////////////////////////////////////////////////////
 
+///////////////////////////  Класс Parser  /////////////////////////////////
+ 
+class Parser {
+    Lex          curr_lex;
+    type_of_lex  c_type;
+    int          c_val;
+    Scanner      scan;
+    stack < int >   st_int;
+    stack < type_of_lex >  st_lex;
+    void  P();
+    void  D1();
+    void  D();
+    void  B();
+    void  S();
+    void  E();
+    void  E1();
+    void  T();
+    void  F();
+    void  dec ( type_of_lex type);
+    void  check_id ();
+    void  check_op ();
+    void  check_not ();
+    void  eq_type ();
+    void  eq_bool ();
+    void  check_id_in_read ();
+    void  gl () {
+              curr_lex = scan.get_lex();
+              c_type = curr_lex.get_type();
+              c_val = curr_lex.get_value();
+    }
+public:
+    vector <Lex> poliz;
+    Parser (const char *program ) : scan (program) { }
+    void  analyze();
+};
+
 
 
 
